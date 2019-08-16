d19ecfb2400a1bb68d115a99785264f9c8ad4001
Qui Le <quile@kms-technology.com>
2019-08-16 17:44:39 +0700

    update

--- katalon/src/scripts/katalon-recoder.sh
+++ katalon/src/scripts/katalon-recoder.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 sleep 5
-ffmpeg -video_size 1024x768 -framerate 25 -f x11grab -i $DISPLAY.0 -c:v libx264 -preset veryfast -crf 23 -pix_fmt yuv420p -f flv rtmp://192.168.1.31:1935/streama/a
+ffmpeg -video_size 1024x768 -framerate 25 -f x11grab -i $DISPLAY.0 -c:v libx264 -preset veryfast -crf 23 -pix_fmt yuv420p -f flv $STREAM_URL
