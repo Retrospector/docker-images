5a1e671d0552c0496ec086bd2bcc16b18aef5dba
Dat Quach <datquach@kms-technology.com>
2020-05-14 09:45:25 +0700

    update aws

---
+++ src/entrypoint_aws.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -xe
+
+echo "Entrypoint"
+
+if [ -z "$KATALON_USER_ID" ]; then
+    exec "$@"
+else
+    echo "Starting with UID : $KATALON_USER_ID"
+    username=katalon
+    useradd --shell /bin/bash -u $KATALON_USER_ID -o -c "" -m $username
+    export HOME=/home/$username
+    chown -R $KATALON_USER_ID $KATALON_SOFTWARE_DIR
+    exec gosu $username "$@"
+    aws meteringmarketplace register-usage --product-code 1z5r0lkbmmrcf8lkanpg2lkn --public-key-version 1
+fi
